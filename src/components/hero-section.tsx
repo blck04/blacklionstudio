24bbc2901f574907e069a8888348774ab8a28b4c
Duck Hombe <146311500+blck04@users.noreply.github.com>
2025-07-03 07:39:03 +0000

    make the main hero cadr with 80 % and the height 95 %

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -5,7 +5,7 @@ export function HeroSection() {
     <section id="home" className="relative w-full h-screen flex flex-col justify-center items-end text-foreground overflow-hidden">
       {/* Card with background image */}
       <div
-        className="relative w-[75vw] h-[85vh] rounded-l-3xl bg-cover bg-center shadow-2xl flex justify-center items-start p-4"
+        className="relative w-[80vw] h-[95vh] rounded-l-3xl bg-cover bg-center shadow-2xl flex justify-center items-start p-4"
         style={{ backgroundImage: "url('/bls-hero.png')" }}
       >
         <NavCard />
