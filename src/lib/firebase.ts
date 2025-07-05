9885a2260b67566c38a7cda945d356857e6f4e83
Duck Hombe <146311500+blck04@users.noreply.github.com>
2025-07-05 06:31:40 +0000

    here is the correct storage bucket  gs://black-mosey-creative-studio.fir

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAYS5rqyaouPtSQH3a6Y4uFC9mwwryCF8c",
   authDomain: "black-mosey-creative-studio.firebaseapp.com",
   projectId: "black-mosey-creative-studio",
-  storageBucket: "black-mosey-creative-studio.appspot.com",
+  storageBucket: "black-mosey-creative-studio.firebasestorage.app",
   messagingSenderId: "1081350771844",
   appId: "1:1081350771844:web:ab5c346f79a0921dfb5ed1"
 };
