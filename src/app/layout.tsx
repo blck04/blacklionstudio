4f183118ab06f55ba3a96e168bc508ed0fad5553
Duck Hombe <146311500+blck04@users.noreply.github.com>
2025-08-27 07:01:34 +0000

    USE THESE FOR THE FAVICON :public/android-chrome-512x512.png public/abou

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,11 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.cdnfonts.com/css/formula-condensed" rel="stylesheet" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body className={cn("font-body antialiased")}>
           <PageWrapper>
