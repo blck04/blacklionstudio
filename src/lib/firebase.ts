8629f17923e5940b66b11f6135764b926248f4db
Duck Hombe <146311500+blck04@users.noreply.github.com>
2025-08-29 08:16:31 +0000

    it seems like we have exposed an api key to the project

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAYS5rqyaouPtSQH3a6Y4uFC9mwwryCF8c",
-  authDomain: "black-mosey-creative-studio.firebaseapp.com",
-  projectId: "black-mosey-creative-studio",
-  storageBucket: "black-mosey-creative-studio.firebasestorage.app",
-  messagingSenderId: "1081350771844",
-  appId: "1:1081350771844:web:ab5c346f79a0921dfb5ed1"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
