945c0db44c75e14c58e4eb5bd1eb2f5f0830bb71
blck04 <146311500+blck04@users.noreply.github.com>
2026-03-26 17:18:43 +0200

    Set up support for the FIREBASE_WEBAPP_CONFIG environment variable

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,14 +5,17 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
+// Fallback to individual env vars if FIREBASE_WEBAPP_CONFIG is not available
+const firebaseConfig = process.env.FIREBASE_WEBAPP_CONFIG 
+  ? JSON.parse(process.env.FIREBASE_WEBAPP_CONFIG)
+  : {
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
